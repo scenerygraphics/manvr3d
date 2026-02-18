4a3976d3c22c0aa9157ce5b6bb92d9f1d036acc4
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2026-02-18 11:57:27 +0100

    MoveInstanceVR: Simplify guarding logic, ensure BDV watcher lock states are correct

--- src/main/kotlin/vr/MoveInstanceVR.kt
+++ src/main/kotlin/vr/MoveInstanceVR.kt
@@ -28,30 +28,37 @@ class MoveInstanceVR(
 
     override fun init(x: Int, y: Int) {
         buttonmanager.pressButton(button, trackerRole)
-        if (!buttonmanager.isTwoHandedActive()) {
-            val pos = getTipPosition()
-            if (manvr3d.mastodon.selectionModel.selectedVertices == null) {
-                manvr3d.selectedSpotInstances.clear()
-                return
-            } else {
-                manvr3d.bdvNotifier?.lockUpdates = true
-                manvr3d.selectedSpotInstances.forEach { inst ->
-                    logger.debug("selected spot instance is $inst")
-                    val spot = manvr3d.geometryHandler.findSpotFromInstance(inst)
-                    val selectedTP = spot?.timepoint ?: -1
-                    if (selectedTP != manvr3d.volumeNode.currentTimepoint) {
-                        manvr3d.selectedSpotInstances.clear()
-                        logger.warn("Tried to move a spot that was outside the current timepoint. Aborting.")
-                        return
-                    } else {
-                        manvr3d.bdvNotifier?.lockUpdates = true
-                        currentControllerPos = manvr3d.sciviewToMastodonCoords(pos)
-                        spot?.let { s ->
-                            adjacentEdges.addAll(s.edges().map { it.internalPoolIndex })
-                            logger.debug("Moving edges $manvr3d.adjacentEdges for spot ${spot.internalPoolIndex}.")
-                        }
-                    }
-                }
+        // Return when a different behavior was intended
+        if (buttonmanager.isTwoHandedActive()) return
+
+        val pos = getTipPosition()
+        // Guard against empty selection
+        if (manvr3d.mastodon.selectionModel.selectedVertices == null
+            || manvr3d.selectedSpotInstances.isEmpty()) {
+            manvr3d.selectedSpotInstances.clear()
+            return
+        }
+
+        // Guard against spots not being in the current timepoint
+        val invalidSpot = manvr3d.selectedSpotInstances.any { inst ->
+            val spot = manvr3d.geometryHandler.findSpotFromInstance(inst)
+            spot?.timepoint != manvr3d.volumeNode.currentTimepoint
+        }
+        if (invalidSpot) {
+            manvr3d.selectedSpotInstances.clear()
+            logger.warn("Tried to move a spot outside the current timepoint. Aborting.")
+            return
+        }
+
+        manvr3d.bdvNotifier?.lockUpdates = true
+        currentControllerPos = manvr3d.sciviewToMastodonCoords(pos)
+
+        manvr3d.selectedSpotInstances.forEach { inst ->
+            logger.debug("selected spot instance is $inst")
+            val spot = manvr3d.geometryHandler.findSpotFromInstance(inst)
+            spot?.let { s ->
+                adjacentEdges.addAll(s.edges().map { it.internalPoolIndex })
+                logger.debug("Moving edges $manvr3d.adjacentEdges for spot ${spot.internalPoolIndex}.")
             }
         }
     }
@@ -59,31 +66,36 @@ class MoveInstanceVR(
     override fun drag(x: Int, y: Int) {
         // Only perform the single hand behavior when no other grab button is currently active
         // to prevent simultaneous execution of behaviors
-        if (!buttonmanager.isTwoHandedActive()) {
-            val pos = getTipPosition()
-            val newPos = manvr3d.sciviewToMastodonCoords(pos)
-            val movement = newPos - currentControllerPos
-            manvr3d.selectedSpotInstances.forEach {
-                it.spatial {
-                    position += movement
-                }
-                manvr3d.geometryHandler.moveSpotInBDV(it, movement)
-            }
-            manvr3d.geometryHandler.mainSpotInstance?.updateInstanceBuffers()
-            manvr3d.geometryHandler.updateLinkTransforms(adjacentEdges)
-            currentControllerPos = newPos
+        if (buttonmanager.isTwoHandedActive()
+            || manvr3d.bdvNotifier?.lockUpdates != true
+            || manvr3d.selectedSpotInstances.isEmpty()
+        ) {
+            return
         }
+        val pos = getTipPosition()
+        val newPos = manvr3d.sciviewToMastodonCoords(pos)
+        val movement = newPos - currentControllerPos
+        manvr3d.selectedSpotInstances.forEach {
+            it.spatial { position += movement }
+            manvr3d.geometryHandler.moveSpotInBDV(it, movement)
+        }
+        manvr3d.geometryHandler.mainSpotInstance?.updateInstanceBuffers()
+        manvr3d.geometryHandler.updateLinkTransforms(adjacentEdges)
+        currentControllerPos = newPos
     }
 
     override fun end(x: Int, y: Int) {
-        if (!buttonmanager.isTwoHandedActive()) {
-            manvr3d.bdvNotifier?.lockUpdates = false
-            manvr3d.geometryHandler.showInstancedSpots(manvr3d.currentTimepoint,
-                manvr3d.currentColorizer)
-            adjacentEdges.clear()
-            manvr3d.bdvNotifier?.lockUpdates = false
-        }
+        val wasLocked = manvr3d.bdvNotifier?.lockUpdates == true
+        manvr3d.bdvNotifier?.lockUpdates = false
         buttonmanager.releaseButton(button, trackerRole)
+
+        if (!buttonmanager.isTwoHandedActive() && wasLocked) {
+            manvr3d.geometryHandler.showInstancedSpots(
+                manvr3d.currentTimepoint,
+                manvr3d.currentColorizer
+            )
+            adjacentEdges.clear()
+        }
     }
 
     companion object {
