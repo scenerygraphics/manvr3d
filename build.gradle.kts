9de7d5f00c536b69fcf970767a7640a7ef1afed4
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2026-06-18 16:42:39 +0200

    build: bump to elephant v0.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 
     implementation("org.slf4j:slf4j-simple:2.0.16")
 
-    implementation("org.elephant:elephant:0.7.0")
+    implementation("org.elephant:elephant:0.8.0")
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
